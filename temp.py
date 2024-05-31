867c263391ab198cf6060b923a773e2829c407fd
Ali0Li <hrcs@mail.ustc.edu.cn>
2024-05-31 18:14:30 +0800

    check the segmentation

--- temp.py
+++ temp.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pickle
+
 synth_covars = np.load(".\\GMM\\synth_covars.npy")
 synth_means = np.load(".\\GMM\\synth_means.npy")
 synth_weights = np.load(".\\GMM\\synth_weights.npy")
@@ -12,4 +14,12 @@ toy_condition_0_seg_bouttypes = np.load(".\\Results\\toy\\toy_condition_0_seg_bo
 toy_condition_0_seg_lengths = np.load(".\\Results\\toy\\toy_condition_0_seg_lengths.npy")
 toy_condition_0_seg_words = np.load(".\\Results\\toy\\toy_condition_0_seg_words.npy")
 
+fw = open(".\\Results\\synth\\synth_condition_0\\BASSresults",'rb')
+data = pickle.load(fw)
+
+words_len = []
+for word in data[2]:
+    words_len.append(len(word))
+
+for
 temp_var4 = np.load(".\\Data\\synth_dataset_condition0.npy")

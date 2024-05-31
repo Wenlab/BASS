d207cffbe06f6f772839e603fcd406b58ed2ce7f
Ali0Li <hrcs@mail.ustc.edu.cn>
2024-05-31 13:26:52 +0800

    add some results and notes

---
+++ temp.py
@@ -0,0 +1,15 @@
+import numpy as np
+synth_covars = np.load(".\\GMM\\synth_covars.npy")
+synth_means = np.load(".\\GMM\\synth_means.npy")
+synth_weights = np.load(".\\GMM\\synth_weights.npy")
+synth_dataset_condition0 = np.load(".\\Data\\synth_dataset_condition0.npy")
+synth_lengths_condition0 = np.load(".\\Data\\synth_lengths_condition0.npy")
+synth_condition_0_seg_bouttypes = np.load(".\\Results\\synth\\synth_condition_0_seg_bouttypes.npy")
+synth_condition_0_seg_lengths = np.load(".\\Results\\synth\\synth_condition_0_seg_lengths.npy")
+synth_condition_0_seg_words = np.load(".\\Results\\synth\\synth_condition_0_seg_words.npy")
+
+toy_condition_0_seg_bouttypes = np.load(".\\Results\\toy\\toy_condition_0_seg_bouttypes.npy")
+toy_condition_0_seg_lengths = np.load(".\\Results\\toy\\toy_condition_0_seg_lengths.npy")
+toy_condition_0_seg_words = np.load(".\\Results\\toy\\toy_condition_0_seg_words.npy")
+
+temp_var4 = np.load(".\\Data\\synth_dataset_condition0.npy")

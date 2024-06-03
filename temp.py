bb641e8590ef0027bd302e3be7939234d184ddd9
Ali0Li <hrcs@mail.ustc.edu.cn>
2024-06-03 22:55:31 +0800

    check the segmentation

--- temp.py
+++ temp.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pickle
+import matplotlib.pyplot as plt
 
 synth_covars = np.load(".\\GMM\\synth_covars.npy")
 synth_means = np.load(".\\GMM\\synth_means.npy")
@@ -17,9 +18,31 @@ toy_condition_0_seg_words = np.load(".\\Results\\toy\\toy_condition_0_seg_words.
 fw = open(".\\Results\\synth\\synth_condition_0\\BASSresults",'rb')
 data = pickle.load(fw)
 
-words_len = []
-for word in data[2]:
-    words_len.append(len(word))
+synth_condition_0_seg_bouttypes_recon = []
+word_start = 0
+for word_start_next in synth_condition_0_seg_lengths:
+    words_end = word_start_next-1
+    word_ID_present = synth_condition_0_seg_words[word_start]
+    for idx_char in range(word_start,word_start_next):
+        if synth_condition_0_seg_words[idx_char] != word_ID_present:
+            print("Inconsistent word! position:",idx_char)
+            print("\n")
+    word_present = data[2][word_ID_present]
+    if len(word_present) != word_start_next-word_start:
+        print("Inconsistent word length! position:", word_start,"length difference:",len(word_present)-(word_start_next-word_start))
+        print("\n")
+    synth_condition_0_seg_bouttypes_recon = np.concatenate((synth_condition_0_seg_bouttypes_recon, word_present))
+    word_start = word_start_next
+print("\n")
+if np.array_equal(synth_condition_0_seg_bouttypes_recon, synth_condition_0_seg_bouttypes):
+    print("seg_bouttypes reconstruction right!")
+    print("\n")
+else:
+    print("seg_bouttypes reconstruction wrong!")
+    print("\n")
 
-for
+unique_elements_in_synth_condition_0_seg_words = np.unique(synth_condition_0_seg_words)
+# plt.hist(synth_condition_0_seg_words, bins=31, color='skyblue', edgecolor='black')
+# plt.show()
+
 temp_var4 = np.load(".\\Data\\synth_dataset_condition0.npy")
